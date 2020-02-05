a8087589b9edc5cd7c3de3160d31aad809c290f9
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-04 21:46:16 -0500

    apply click event to entire poster

--- js/clock.js
+++ js/clock.js
@@ -10,12 +10,16 @@ console.log(clock)
 console.log(plays)
 console.log(controls)
 
-controls.forEach(control =>
-  control.addEventListener('click', e => {
-    console.log(e)
-    plays.forEach(playing => playing.classList.toggle('paused'))
-  })
-)
+// controls.forEach(control =>
+//   control.addEventListener('click', e => {
+//     console.log(e)
+//     plays.forEach(playing => playing.classList.toggle('paused'))
+//   })
+// )
+
+poster.addEventListener('click', e => {
+  plays.forEach(playing => playing.classList.toggle('paused'))
+})
 
 // pause.addEventListener('click', e => {
 //   console.log(e)
