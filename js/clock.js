72773536f95e67f8acc285b28f5a7ea7917b2152
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-04 21:19:01 -0500

    basic play/pause interaction

--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,19 @@ const plays = document.querySelectorAll('.plays')
 
 const clock = document.getElementById('clock')
 
+const poster = document.getElementById('futura')
+
+console.log(poster)
 console.log(clock)
 console.log(plays)
 console.log(controls)
+
+controls.forEach(control =>
+  control.addEventListener('click', e => {
+    console.log(e)
+    plays.forEach(playing => playing.classList.toggle('paused'))
+  })
+)
 
 // pause.addEventListener('click', e => {
 //   console.log(e)
