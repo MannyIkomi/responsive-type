22065cb25679b857aa784bcfcd5e291a265d6733
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2020-02-05 21:51:36 -0500

    adjust sylabus padding on larger viewports

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,7 @@
 const controls = document.querySelectorAll('button.control')
 const animating = document.querySelectorAll('.playing')
 const poster = document.getElementById('futura')
-
 const clock = document.getElementById('clock')
-
-console.log(poster)
-console.log(clock)
-console.log(animating)
-console.log(controls)
 
 controls.forEach(control =>
   control.addEventListener('click', e => {
@@ -21,18 +15,3 @@ controls.forEach(control =>
     clock.classList.toggle('toggle-color')
   })
 )
-
-// poster.addEventListener('click', e => {
-//   animating.forEach(animating => {
-//     animating.classList.toggle('paused')
-//   })
-//   controls.forEach(control => {
-//     // control.classList.contains('switch') &&
-//     control.classList.toggle('toggle-color')
-//   })
-// })
-
-// pause.addEventListener('click', e => {
-//   console.log(e)
-//   pairs.classList.toggle('paused')
-// })
